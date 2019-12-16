8e94c5090e260d4a5a9ca9df879cfec3990dd206
imeepos <1037483576@qq.com>
2019-12-16 16:07:18 +0800

    fix orm

--- packages/node/lib/index.ts
+++ packages/node/lib/index.ts
@@ -1,6 +1,8 @@
 import { corePlatform, createPlatformFactory, LOGGER_LEVEL, LogLevel } from '@nger/core';
 import { loggerProvider } from './logger';
+import { ngerOrmCoreHandlers } from '@nger/orm.core'
 export const platformNode = createPlatformFactory(corePlatform, 'node', [
+    ...ngerOrmCoreHandlers,
     {
         provide: LOGGER_LEVEL,
         useValue: 'error' as LogLevel,
