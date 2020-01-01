222e377d827f5827b4669aa5d83ee205cb95e16f
imeepos <admin@meepo.com.cn>
2020-01-01 23:55:10 +0800

    dep main path

--- packages/node/lib/config.ts
+++ packages/node/lib/config.ts
@@ -1,10 +1,14 @@
-import { Config } from '@nger/core';
+import { Config, MAIN_PATH, Injector, isDevMode } from '@nger/core';
 import { config } from 'dotenv';
-
+export function getEnvPath() { }
 export class EnvConfig extends Config {
-    constructor() {
+    constructor(private injector: Injector) {
         super();
-        config();
+        config({
+            path: injector.get<string>(MAIN_PATH),
+            encoding: 'utf8',
+            debug: isDevMode()
+        });
     }
     get<T = any>(key: string, def: T) {
         return Reflect.get(process.env, key) || def;
